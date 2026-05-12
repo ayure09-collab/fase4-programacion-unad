738ef209612e63d9621a2d4dc9ecfb82aa635f7a
ayure09-collab <ayure09@gmail.com>
2026-05-11 20:58:54 -0500

    Create excepciones.py

---
+++ excepciones.py
@@ -0,0 +1,13 @@
+class ClienteInvalido(Exception):
+    """Excepción para clientes inválidos"""
+    pass
+
+
+class ServicioNoDisponible(Exception):
+    """Excepción para servicios inválidos"""
+    pass
+
+
+class ReservaError(Exception):
+    """Excepción general para reservas"""
+    pass
